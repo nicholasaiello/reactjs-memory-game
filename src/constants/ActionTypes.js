485e584138413e4c65b34934a9d491c8a74348a6
Nicholas Aiello <nikko.aiello@gmail.com>
2017-10-12 22:42:20 -0500

    start adding redux

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,14 @@
+/**
+ * Game actions
+ */
+export const NEW_GAME = 0;
+export const END_GAME = 1;
+export const WON_GAME = 2;
+
+export const DEFAULT_CARD = 3;
+export const FLIP_CARD = 4;
+export const MATCH_CARD = 5;
+export const NO_MATCH_CARD = 6;
+
+export const UPDATE_STATS_ATTEMPTS = 7;
+export const UPDATE_STATS_MATCHES = 7;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components/App';
 
-/**
- *
- */
+import reducer from './reducers';
+
+import './index.css';
+
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-	<MuiThemeProvider>
-		<App />
-	</MuiThemeProvider>, document.getElementById('app-root')
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>, document.getElementById('app-root')
 );
