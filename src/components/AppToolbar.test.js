8b376d94a97f86c2e21b8a3c20796bbc03504c38
Nicholas Aiello <nikko.aiello@gmail.com>
2017-10-29 12:35:02 -0500

    refactor Deck tests, add snapshot tests for AppToolbar, add circleci

---
+++ src/components/AppToolbar.test.js
@@ -0,0 +1,39 @@
+import assert from 'assert';
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import * as GameStates from '../constants/GameStates';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppToolbar from './AppToolbar';
+
+describe('AppToolbar', () => {
+
+  it('renders toolbar for inactive game', () => {
+    const state = {state: GameStates.INACTIVE, prevState: GameStates.INACTIVE};
+    const component = renderer.create(
+      <MuiThemeProvider>
+        <AppToolbar stats={{}} gameState={state} onStartClick={() => {}} onEndClick={() => {}} />
+      </MuiThemeProvider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders toolbar for active game', () => {
+    const stats = {attempts: 0, matches: 0};
+    const state = {state: GameStates.ACTIVE, prevState: GameStates.ACTIVE};
+
+    const component = renderer.create(
+      <MuiThemeProvider>
+        <AppToolbar stats={stats} gameState={state} onStartClick={() => {}} onEndClick={() => {}} />
+      </MuiThemeProvider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
--- src/components/Deck.test.js
+++ src/components/Deck.test.js
@@ -3,51 +3,55 @@ import assert from 'assert';
 import React from 'react';
 import Deck from './Deck';
 
-it('init deck with N cards', () => {
-  const n = 12;
-  const d = Deck.create(n);
+describe('Deck of Cards', () => {
+  
+  it('init deck with N cards', () => {
+    const n = 12;
+    const d = Deck.create(n);
 
-  assert( d.size === n );
-});
-
-it('deal deck of shuffled', () => {
-  const n = 12;
-  const d = Deck.create(n);
-
-  d.deal(true);
-
-  assert( d.size === n );
-});
-
-it('deck shouldn\'t be equal after shuffling', () => {
-  const d = Deck.create(12),
-    shouldShuffle = true;
-
-  // deal() creates a new array
-  d.deal(shouldShuffle);
-  const first = d.cards;
-
-  d.deal(shouldShuffle);
-  const second = d.cards;
-
-  assert( !(first.toString() === second.toString()) );
-});
-
-it('matchSetSize should correctly set card values, size / matchSetSize', () => {
-  const size = 12,
-    matchSetSize = 2;
-
-  const d = Deck.create(size, matchSetSize),
-    shouldShuffle = false;
-
-  let arr = [];
-  d.deal(shouldShuffle);
-
-  d.cards.map((v) => {
-    if (arr.indexOf(v) === -1) {
-      arr.push(v);
-    }
+    assert( d.size === n );
   });
 
-  assert( arr.length === (size / matchSetSize) );
+  it('deal deck of shuffled', () => {
+    const n = 12;
+    const d = Deck.create(n);
+
+    d.deal(true);
+
+    assert( d.size === n );
+  });
+
+  it('deck shouldn\'t be equal after shuffling', () => {
+    const d = Deck.create(12),
+      shouldShuffle = true;
+
+    // deal() creates a new array
+    d.deal(shouldShuffle);
+    const first = d.cards;
+
+    d.deal(shouldShuffle);
+    const second = d.cards;
+
+    assert( !(first.toString() === second.toString()) );
+  });
+
+  it('matchSetSize should correctly set card values, size / matchSetSize', () => {
+    const size = 12,
+      matchSetSize = 2;
+
+    const d = Deck.create(size, matchSetSize),
+      shouldShuffle = false;
+
+    let arr = [];
+    d.deal(shouldShuffle);
+
+    d.cards.map((v) => {
+      if (arr.indexOf(v) === -1) {
+        arr.push(v);
+      }
+    });
+
+    assert( arr.length === (size / matchSetSize) );
+  });
+
 });
